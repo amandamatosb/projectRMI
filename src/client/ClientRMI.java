adb17efc4283b40e6325a4f74ab2b80048263960
mand <amandabarrosmatoss@gmail.com>
2026-04-09 17:54:35 -0400

    feat: add synchronized list and test

--- src/client/ClientRMI.java
+++ src/client/ClientRMI.java
@@ -1,13 +1,26 @@
 package client;
 
 import interfaces.IWhiteboard;
+import interfaces.Stroke;
+
+import java.awt.*;
 import java.rmi.Naming;
+
+import static java.awt.Color.blue;
 
 public class ClientRMI {
     public static void main(String[] args) throws Exception {
         String objName = "rmi://localhost:1099/Board";
         IWhiteboard board = (IWhiteboard) Naming.lookup(objName);
 
+        Stroke firstline = new Stroke(Color.blue, 2);
+        firstline.addPoint(1,2);
+        firstline.addPoint(1, 4);
+        firstline.addPoint(1, 6);
+
+        board.sendStroke(firstline);
+
+        System.out.println("All strokes: " + board.getAllStrokes().size());
 
     }
 }
--- src/server/Whiteboard.java
+++ src/server/Whiteboard.java
@@ -5,19 +5,22 @@ import interfaces.Stroke;
 
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Whiteboard extends UnicastRemoteObject implements IWhiteboard{
 
     public Whiteboard() throws RemoteException {}
+    List<Stroke> synchronizedList = Collections.synchronizedList(new ArrayList<>());
 
     @Override
     public void sendStroke(Stroke s) throws RemoteException {
-
+        synchronizedList.add(s);
     }
 
     @Override
     public List<Stroke> getAllStrokes() throws RemoteException {
-        return List.of();
+        return new ArrayList<>(synchronizedList);
     }
 }
